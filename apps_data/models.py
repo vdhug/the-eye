a4bf8ffb15b15d3312248fea9de7a459346a54de
Vitor Hugo da Silva Lima <silvalimavitorhugo@gmail.com>
2022-04-09 16:43:55 -0500

    feat(apps_data): Starting modeling application that will be responsible for managing the apps data

--- apps_data/models.py
+++ apps_data/models.py
@@ -1,3 +1,27 @@
+from uuid import UUID, uuid4
+from datetime import datetime
 from django.db import models
 
-# Create your models here.
+
+class Application(models.Model):
+    name: str = models.CharField(max_length=300, default=None, blank=True, null=True)
+
+
+class Session(models.Model):
+    application: Application = models.ForeignKey(
+        Application, related_name="sessions", on_delete=models.CASCADE, blank=True, null=True
+    )
+    uuid: UUID = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+
+
+class Event(models.Model):
+    session: Session = models.ForeignKey(
+        Session, related_name="events", on_delete=models.CASCADE, blank=True, null=True
+    )
+    name: str = models.CharField(max_length=300, default=None, blank=True, null=True)
+    category: str = models.CharField(max_length=300, default=None, blank=True, null=True)
+    payload: dict = models.JSONField()
+    timestamp: datetime = models.DateTimeField()
+
+    class Meta:
+        ordering = ["-timestamp"]
