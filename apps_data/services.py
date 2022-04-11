41af72141cc6b25144a9b2fe800eaf0734478412
Vitor Hugo da Silva Lima <silvalimavitorhugo@gmail.com>
2022-04-11 10:57:31 -0500

    feat(apps_data): Creating service responsible for register a new event

--- apps_data/services.py
+++ apps_data/services.py
@@ -1,9 +1,11 @@
 from uuid import UUID
+from datetime import datetime
 
-from apps_data.dataclasses import ApplicationData, SessionData
+from apps_data.dataclasses import ApplicationData, EventData, SessionData
 from apps_data import providers as apps_data_providers
 from apps_data.models import Application, Session
 from apps_data.utils import build_dataclass_from_model_instance
+from apps_data.validators import payload_validation
 
 
 def get_or_create_application_by_name(application_name: str) -> ApplicationData:
@@ -32,3 +34,26 @@ def get_or_create_session_by_application_name_and_session_id(
         klass=SessionData, instance=session, application=application_data
     )
     return application_data
+
+
+def register_event(session_id: UUID, category: str, name: str, payload: dict, timestamp: datetime):
+    payload_validation(category=category, name=name, payload=payload)
+    application_name = payload.get("host")
+
+    if not session_id or not category or not name or not application_name or not timestamp:
+        raise ValueError
+    session_data = get_or_create_session_by_application_name_and_session_id(
+        application_name=application_name, session_id=session_id
+    )
+    event = apps_data_providers.create_event(
+        session_uuid=session_data.uuid,
+        name=name,
+        category=category,
+        payload=payload,
+        timestamp=timestamp,
+    )
+
+    event_data = build_dataclass_from_model_instance(
+        klass=EventData, instance=event, session=session_data
+    )
+    return event_data
