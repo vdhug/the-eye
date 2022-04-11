0c86609200dc7a166f31ec7eca4bedb5365398a2
Vitor Hugo da Silva Lima <silvalimavitorhugo@gmail.com>
2022-04-11 09:58:39 -0500

    test(apps_data): Adding unittests to service get_or_create_application_by_name

--- apps_data/tests/test_services.py
+++ apps_data/tests/test_services.py
@@ -76,3 +76,59 @@ def test_mock_get_application_by_name__application_does_not_exists(
         mock_get_application_by_name.assert_called_once_with(
             application_name=application_name,
         )
+
+
+@mock.patch("apps_data.services.apps_data_providers.get_application_by_name")
+@mock.patch("apps_data.services.build_dataclass_from_model_instance")
+def test_mock_get_or_create_application__application_exists(
+    mock_build_dataclass_from_model_instance, mock_get_application_by_name
+):
+    # Argument
+    application_name = mock.Mock()
+
+    # Setup
+    application = mock.Mock()
+    application_data = mock.Mock()
+
+    mock_get_application_by_name.return_value = application
+    mock_build_dataclass_from_model_instance.return_value = application_data
+
+    # Execution
+    result = apps_data_services.get_or_create_application_by_name(application_name=application_name)
+
+    # Assertions
+    mock_get_application_by_name.assert_called_once_with(
+        application_name=application_name,
+    )
+    mock_build_dataclass_from_model_instance.assert_called_once()
+
+    assert result == application_data
+
+
+@mock.patch("apps_data.services.apps_data_providers.get_application_by_name")
+@mock.patch("apps_data.services.apps_data_providers.create_application")
+@mock.patch("apps_data.services.build_dataclass_from_model_instance")
+def test_mock_get_or_create_application__application_does_not_exists(
+    mock_build_dataclass_from_model_instance, mock_create_application, mock_get_application_by_name
+):
+    # Argument
+    application_name = mock.Mock()
+
+    # Setup
+    application = mock.Mock()
+    application_data = mock.Mock()
+
+    mock_get_application_by_name.side_effect = Application.DoesNotExist()
+    mock_create_application.return_value = application
+    mock_build_dataclass_from_model_instance.return_value = application_data
+
+    # Execution
+    result = apps_data_services.get_or_create_application_by_name(application_name=application_name)
+
+    # Assertions
+    mock_get_application_by_name.assert_called_once_with(
+        application_name=application_name,
+    )
+    mock_build_dataclass_from_model_instance.assert_called_once()
+
+    assert result == application_data
