c2591405d68eab28985228078366469b49c492a7
Vitor Hugo da Silva Lima <silvalimavitorhugo@gmail.com>
2022-04-11 10:58:03 -0500

    test(apps_data): Adding unittests to new service register_event

--- apps_data/tests/test_services.py
+++ apps_data/tests/test_services.py
@@ -135,3 +135,60 @@ def test_mock_get_or_create_session_by_application_name_and_session_id__session_
     mock_build_dataclass_from_model_instance.assert_called_once()
 
     assert result == session_data
+
+
+@mock.patch("apps_data.services.payload_validation")
+@mock.patch("apps_data.services.get_or_create_session_by_application_name_and_session_id")
+@mock.patch("apps_data.services.apps_data_providers.create_event")
+@mock.patch("apps_data.services.build_dataclass_from_model_instance")
+def test_register_event(
+    mock_build_dataclass_from_model_instance,
+    mock_create_event,
+    mock_get_or_create_session_by_application_name_and_session_id,
+    mock_payload_validation,
+):
+    # Arguments
+    session_id = mock.Mock()
+    category = mock.Mock()
+    name = mock.Mock()
+    payload = mock.MagicMock()
+    timestamp = mock.Mock()
+
+    session_data = mock.Mock()
+    event = mock.Mock()
+    event_data = mock.Mock()
+
+    # Setup
+    payload.__getitem__.side_effect = {"host": "www.consumeraffairs.com"}.__getitem__
+    mock_payload_validation.return_value = None
+    mock_get_or_create_session_by_application_name_and_session_id.return_value = session_data
+    mock_create_event.return_value = event
+    mock_build_dataclass_from_model_instance.return_value = event_data
+
+    # Execution
+    result = apps_data_services.register_event(
+        session_id=session_id, category=category, name=name, payload=payload, timestamp=timestamp
+    )
+
+    # Assertions
+    mock_payload_validation.assert_called_once_with(
+        category=category,
+        name=name,
+        payload=payload,
+    )
+
+    mock_get_or_create_session_by_application_name_and_session_id.assert_called_once_with(
+        application_name=payload.get("host"), session_id=session_id
+    )
+
+    mock_create_event.assert_called_once_with(
+        session_uuid=session_data.uuid,
+        name=name,
+        category=category,
+        payload=payload,
+        timestamp=timestamp,
+    )
+
+    mock_build_dataclass_from_model_instance.assert_called_once()
+
+    assert result == event_data
