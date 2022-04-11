7eec6314dd7d36f5d8861c5fe17e104e12bbd9b7
Vitor Hugo da Silva Lima <silvalimavitorhugo@gmail.com>
2022-04-10 21:52:36 -0500

    feat(apps_data): Creating new provider responsible for retrieving an application by id

--- apps_data/services.py
+++ apps_data/services.py
@@ -1,10 +1,24 @@
 from apps_data.dataclasses import ApplicationData
 from apps_data import providers as apps_data_providers
+from apps_data.exceptions import ApplicationDoesNotExists
+from apps_data.models import Application
 from apps_data.utils import build_dataclass_from_model_instance
 
 
 def create_application(name: str) -> ApplicationData:
     application = apps_data_providers.create_application(name=name)
+    application_data = build_dataclass_from_model_instance(
+        klass=ApplicationData,
+        instance=application,
+    )
+    return application_data
+
+
+def get_application_by_id(application_id: int) -> ApplicationData:
+    try:
+        application = apps_data_providers.get_application_by_id(application_id=application_id)
+    except Application.DoesNotExist:
+        raise ApplicationDoesNotExists
     application_data = build_dataclass_from_model_instance(
         klass=ApplicationData,
         instance=application,
